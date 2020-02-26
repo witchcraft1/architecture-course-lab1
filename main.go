f703fbf5d9e8f9a0b6296ea9be142de55cfa9f8b
Dan <51187729+DaniilDenisyuk@users.noreply.github.com>
2020-02-26 21:39:22 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	Lab1 "github.com/witchcraft1/architecture-course-laba1"
+	"log"
+)
+
+func main() {
+	//args := os.Args[1:]
+	//res, err := Lab1.PrefixToInfix(args[0])
+	res, err := Lab1.PrefixToInfix("+ * 2 2 * - 10 2 5")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+	fmt.Println(buildVersion)
+}
